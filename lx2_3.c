7c14ace7a09cb07a0bae876a1d22ba0e5ee122c8
lisi <si13579@163.com>
2020-08-04 18:57:00 +0800

    20190804

---
+++ lx2_3.c
@@ -0,0 +1,10 @@
+/*
+ * @Date: 2020-08-03 23:16:45
+编写函数htoi(s),把由十六进制数字组成的字符串（包含可选的前缀0x或0X)转换为
+与之等价的整型值。字符串中允许的数字包括:0~9,a~f,以及A~F.
+ */
+#include<stdio.h>
+#define YES 1
+#define NO  0
+//htoi:convert headecimal string s to integer
+http://dict.youdao.com/search?q=%GDWORD%&ue=utf8
